0494ea0a58257a943ecb5fe117ea291d028fc5e2
Youssef Ismail <yismail@ualberta.ca>
2024-03-04 16:26:46 -0700

    Fix api client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,37 +1,7 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export { ApiError } from './core/ApiError';
-export { CancelablePromise, CancelError } from './core/CancelablePromise';
-export { OpenAPI } from './core/OpenAPI';
-export type { OpenAPIConfig } from './core/OpenAPI';
+import createClient from 'openapi-fetch'
+import type { paths } from "./api";
 
-export type { BaseCreateableTask } from './models/BaseCreateableTask';
-export type { BaseUpdateableTask } from './models/BaseUpdateableTask';
-export type { CreatableUser } from './models/CreatableUser';
-export type { CreateableMilestone } from './models/CreateableMilestone';
-export type { CreateableProject } from './models/CreateableProject';
-export type { CreateableSprint } from './models/CreateableSprint';
-export type { CreateableTask } from './models/CreateableTask';
-export type { HTTPValidationError } from './models/HTTPValidationError';
-export type { Milestone } from './models/Milestone';
-export type { Priority } from './models/Priority';
-export type { Project } from './models/Project';
-export type { ProjectView } from './models/ProjectView';
-export type { Sprint } from './models/Sprint';
-export type { Status } from './models/Status';
-export type { Task } from './models/Task';
-export type { UpdateableMilestone } from './models/UpdateableMilestone';
-export type { UpdateableProject } from './models/UpdateableProject';
-export type { UpdateableSprint } from './models/UpdateableSprint';
-export type { UpdateableTask } from './models/UpdateableTask';
-export type { User } from './models/User';
-export type { UserView } from './models/UserView';
-export type { ValidationError } from './models/ValidationError';
 
-export { MilestonesService } from './services/MilestonesService';
-export { ProjectsService } from './services/ProjectsService';
-export { SprintsService } from './services/SprintsService';
-export { TasksService } from './services/TasksService';
-export { UsersService } from './services/UsersService';
+const client = createClient<paths>({baseUrl: 'http://162.246.157.194/'})
+
+export default client;
--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,39 +1,43 @@
-import {
-    VStack,
-    Stack,
-    Heading,
-    Button,
-} from "@chakra-ui/react"
+import { VStack, Stack, Heading, Button } from "@chakra-ui/react";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { TextField } from "./TextField"
+import { TextField } from "./TextField";
 import { useNavigate } from "react-router-dom";
+import client from "../client";
 
 export function RegistrationForm() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-      <Formik
-        initialValues={{
-            username: "",
-            email: "",
-            password: "",
-        }}
-        validationSchema={Yup.object({
-            username: Yup.string().required("Username required"),
-            email: Yup.string().required("Email required"),
-            password: Yup.string().required("Password required").min(8, "Password must be at least 8 characters")
-        })}
-        onSubmit= { (values, actions) => {
-            alert(JSON.stringify(values, null, 3));
-            actions.resetForm();
-        }}
-      >
-        {formik => (
-          <form onSubmit={formik.handleSubmit}>
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+      }}
+      validationSchema={Yup.object({
+        username: Yup.string().required("Username required"),
+        email: Yup.string().required("Email required"),
+        password: Yup.string()
+          .required("Password required")
+          .min(8, "Password must be at least 8 characters"),
+      })}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 3));
+        actions.resetForm();
+
+        const res = client.POST("/users/register", {
+          body: values,
+        });
+
+        console.log(res);
+      }}
+    >
+      {(formik) => (
+        <form onSubmit={formik.handleSubmit}>
           <VStack
             mx="auto"
-            w={{ base: "90%", md: 500}}
+            w={{ base: "90%", md: 500 }}
             h="100vh"
             justifyContent="center"
           >
@@ -47,7 +51,7 @@ export function RegistrationForm() {
               variant="filled"
               placeholder="enter username..."
             />
-  
+
             <TextField
               id="email"
               name="email"
@@ -56,7 +60,7 @@ export function RegistrationForm() {
               variant="filled"
               placeholder="enter email..."
             />
-  
+
             <TextField
               id="password"
               name="password"
@@ -66,18 +70,22 @@ export function RegistrationForm() {
               placeholder="enter password..."
             />
 
-            <Stack spacing={4} direction='row' align='center'>
-                <Button colorScheme="teal" variant="solid" onClick={() => navigate("/")}>
+            <Stack spacing={4} direction="row" align="center">
+              <Button
+                colorScheme="teal"
+                variant="solid"
+                onClick={() => navigate("/")}
+              >
                 Back
-                </Button>
-    
-                <Button type="submit" colorScheme="teal" variant="solid">
+              </Button>
+
+              <Button type="submit" colorScheme="teal" variant="solid">
                 Create Account
-                </Button>
+              </Button>
             </Stack>
           </VStack>
         </form>
-        )}
-      </Formik>
-    );
+      )}
+    </Formik>
+  );
 }
