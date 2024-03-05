a7b193e51e662e31e340dfc085903d72d0621ea9
ElenaArnold <earnold@ualberta.ca>
2024-03-05 16:57:45 -0700

    Added an error message for invalid credentials

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,19 +3,27 @@ import {
     Stack,
     Heading,
     Button,
+    Box,
+    Text
 } from "@chakra-ui/react"
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { TextField } from "./TextField"
 import { useNavigate, redirect } from "react-router-dom";
 import { createClientWithToken } from "../client";
+import { SetStateAction, useState } from "react";
 
 export function LoginForm({onAuthenticate}: any) {
+    const [errorMessage, setErrorMessage] = useState("");
+
     const navigate = useNavigate();
     const client = createClientWithToken(null);
+    const displayErrorMessage = () => {
+        setErrorMessage("Invalid username or password")
+    }
 
     return (
-      <Formik
+    <Formik
         initialValues={{
             username: "",
             password: "",
@@ -29,64 +37,67 @@ export function LoginForm({onAuthenticate}: any) {
 
             // Query if the user credentials are valid
             const {data, error, response} = await client.POST("/users/login", {
-              params: {
+            params: {
                 query: values
-              },
+            },
             });
 
-            // If the credentials are invalid
+            // If the credentials are invalid diplay an error message to the user
             if (error) {
-              console.log(error);
-              // If the response is valid authenticate the user and naviagte to the project list page
+                console.log(error);
+                displayErrorMessage();
+            // If the response is valid authenticate the user and naviagte to the project list page
             } else if (response.status == 200) {
-              onAuthenticate(data.token);
-              actions.resetForm();
-              navigate("/projectlist");
+                onAuthenticate(data.token);
+                actions.resetForm();
+                navigate("/projectlist");
             } else {
-              console.log(response);
+                console.log(response);
             }          
         }}
-      >
+    >
         {formik => (
-          <form onSubmit={formik.handleSubmit}>
-          <VStack
-            mx="auto"
-            w={{ base: "90%", md: 500}}
-            h="100vh"
-            justifyContent="center"
-          >
-            <Heading>Login</Heading>
-  
-            <TextField
-              id="username"
-              name="username"
-              label="Username"
-              type="username"
-              variant="filled"
-              placeholder="enter username..."
-            />
-  
-            <TextField
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              variant="filled"
-              placeholder="enter password..."
-            />
+            <form onSubmit={formik.handleSubmit}>
+                <VStack
+                    mx="auto"
+                    w={{ base: "90%", md: 500}}
+                    h="100vh"
+                    justifyContent="center"
+                >
+                    <Heading>Login</Heading>
 
-            <Stack spacing={4} direction='row' align='center'>
-                <Button colorScheme="teal" variant="solid" onClick={() => navigate("/")}>
-                Back
-                </Button>
+                    <TextField
+                        id="username"
+                        name="username"
+                        label="Username"
+                        type="username"
+                        variant="filled"
+                        placeholder="enter username..."
+                    />
 
-                <Button type="submit" colorScheme="teal" variant="solid">
-                Login
-                </Button>
-            </Stack>
-          </VStack>
-        </form>
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Password"
+                        type="password"
+                        variant="filled"
+                        placeholder="enter password..."
+                    />
+
+                    {errorMessage && <Box><Text color='tomato'> {errorMessage} </Text></Box>}
+
+                    <Stack spacing={4} direction='row' align='center'>
+                        <Button colorScheme="teal" variant="solid" onClick={() => navigate("/")}>
+                        Back
+                        </Button>
+
+                        <Button type="submit" colorScheme="teal" variant="solid">
+                        Login
+                        </Button>
+                    </Stack>
+                </VStack>
+            </form>
         )}
-      </Formik>
+    </Formik>
     );
 }
