c00888c123c6164c2f23dacad2bcab30982fbef1
ElenaArnold <earnold@ualberta.ca>
2024-03-13 17:10:30 -0600

    Added error & success messages to add member

--- src/components/AddMemberModal.tsx
+++ src/components/AddMemberModal.tsx
@@ -10,6 +10,7 @@ export function AddMemberModal({fetchProjectMembers, isOpen, onClose}: any) {
   const client = useContext(ApiContext).client;
   const project = useContext(ApiContext).project;
   const navigate = useNavigate();
+  const toast = useToast();
 
   if (!project) {
     navigate("/projectlist");
@@ -44,12 +45,32 @@ export function AddMemberModal({fetchProjectMembers, isOpen, onClose}: any) {
                 },
               });
 
+              // If there is an error adding the member to the project notify the user with a toast message
               if (error) {
                 console.log(error);
+                toast({
+                  title: "Add Project Member Failed",
+                  description: error.detail?.toString() ?
+                  error.detail?.toString() + ". Please verify the email is correct and the user is registered with the system." :
+                  "There was an error adding the member to the project. Please verify the email is correct and the user is registered with the system.",
+                  status: "error",
+                  duration: 8000,
+                  isClosable: true,
+                  position: "top",
+                });
+              // If the response is valid fetch the new list of project members and notify the user with a success toast message
               } else if (response.status === 200) {
                 console.log(data);
-                fetchProjectMembers();
+                fetchProjectMembers(); // Update the list of project members
                 onClose();
+                toast({
+                  title: "Successfully Added Member To Project",
+                  description: data.username + " has been successfully added to the project",
+                  status: "success",
+                  duration: 8000,
+                  isClosable: true,
+                  position: "top",
+                });
               } else {
                 console.log(response);
               }
